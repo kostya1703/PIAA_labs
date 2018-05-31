7b19eab68b5fdec207f494beb60b84b13bc7f2db
kostya1703 <knock1703@gmail.com>
2018-05-31 23:24:58 +0300

    Update func.h

--- lab6/hdr/func.h
+++ lab6/hdr/func.h
@@ -8,32 +8,38 @@
 #include <vector>
 #include <map>
 #include <sstream>
+#include <clocale>
+
+
+
 
 
 using namespace std;
+using std::wstring;
+
 
 struct Bohr{
-    int parent; //parent - вершина-отец в дереве
-    int suff_link; //suff_link - суффиксная ссылка
+    size_t parent; //parent - вершина-отец в дереве
+    int suff_link; //suff_link - суф ссылка
     int suff_flink; //suff_flink - "хорошая" суф. ссылка
     bool flag; //flag — бит, указывающий на то, является ли наша вершина исходной строкой
-    char symb; //символ на ребре от parent к этой вершине
-    map<char,int> nextV; //nextV[i] — номер вершины, в которую мы придем по символу с номером i в алфавите
-    vector<int> N_pattern; //pat_num — номер строки-образца, обозначаемого этой вершиной
-    map<char,int> auto_move; //auto_move - запоминание перехода автомата
+    wchar_t symb; //символ на ребре от parent к этой вершине
+    map<wchar_t,int> nextV; //nextV[i] — номер вершины, в которую мы придем по символу с номером i в алфавите
+    vector<int> N_pattern; //N_pattern — номер строки-образца, обозначаемого этой вершиной
+    map<wchar_t,int> auto_move; //auto_move - запоминание перехода автомата
 };
 
-Bohr makeBohr(int p, char c);
+Bohr makeBohr(int p, wchar_t c);
 void initBohr();
-void addstrBohr(const string& s, vector<string>& pattern);
-int getAuto_move(int v, char ch);
-int get_Suff_link(int v);
-int get_Suff_flink(int v);
-void stand_check(int v,int i,const vector<string>& pattern, map<int,vector<int>>& answ);
-void joker_check(int v,int i,vector<int>& count, const vector<int>& spliter);
-void stand_find_all_pos(const string& s,const vector<string>& pattern, map<int,vector<int>>& answ);
-void joker_find_all_pos(const string& s,vector<int>& count, const vector<int>& spliter);
-vector<int> split(stringstream& str_pat, char razd, vector<string>& pattern);
+void addstrBohr(const wstring& s, vector<wstring>& pattern);
+int getAuto_move(size_t v, wchar_t ch);
+int get_Suff_link(size_t v);
+int get_Suff_flink(size_t v);
+void stand_check(size_t v,int i,const vector<wstring>& pattern, map<int,vector<int>>& answ);
+void joker_check(size_t v,int i,vector<int>& count, const vector<int>& spliter);
+void stand_find_all_pos(const wstring& s,const vector<wstring>& pattern, map<int,vector<int>>& answ);
+void joker_find_all_pos(const wstring& s,vector<int>& count, const vector<int>& spliter);
+vector<int> split(wstringstream& str_pat, wchar_t razd, vector<wstring>& pattern);
 vector<int> getPos(const vector<int>& count, int endd, int pattern_size);
 
 #endif
